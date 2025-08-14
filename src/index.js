9383e188b1eb58c726834d6748a612ea10c4cbc9
nokhwezi <nokhwezi13@gmail.com>
2025-08-14 04:07:18 +0200

    added axios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 function generatePoem(e) {
   e.preventDefault();
 
+  let apiKey = "ob2cf76b441t303bab3624054158f86a";
+  let prompt = "";
+  let context = "";
+  let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios
+
   new Typewriter("#poem", {
     strings: "La tombe dit à la rose",
     autoStart: true,
